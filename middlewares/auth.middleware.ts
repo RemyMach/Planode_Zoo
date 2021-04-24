e2f803d1d046c1c85c82c48448cf436cce03ef97
RemyMach <remygt@hotmail.fr>
2021-04-24 10:13:25 +0200

    delete auth.middleware a user middleware

--- middlewares/auth.middleware.ts
+++ middlewares/auth.middleware.ts
@@ -60,23 +60,3 @@ export async function superAdminAuthMiddleware(req: express.Request, res: expres
         res.status(401).end();
     }
 }
-
-export async function veterinaryAuthMiddleware(req: express.Request, res: express.Response, next: express.NextFunction) {
-    const auth = req.headers["authorization"];
-    
-    if(auth !== undefined) {
-        const token = auth.replace('Bearer ', '');
-        
-        const authController = await AuthController.getInstance();
-        const session = await authController.getSpecificRoleSession(token, ['super_admin']);
-        if(session !== null) {
-            next();
-            return;
-        } else {
-            res.status(403).end();
-            return;
-        }
-    } else {
-        res.status(401).end();
-    }
-}
